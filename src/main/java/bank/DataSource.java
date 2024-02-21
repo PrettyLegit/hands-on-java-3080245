88b6d8b5af21a890c37604cb5c4ec90b5540f6c1
Jimmy Nguyen <nvjimmy27@gmail.com>
2024-02-21 22:41:03 +0000

    Construct customer from data source part 1

--- src/main/java/bank/DataSource.java
+++ src/main/java/bank/DataSource.java
@@ -1,6 +1,8 @@
 package bank;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
 public class DataSource {
   
@@ -17,7 +19,32 @@ public class DataSource {
     return connection;
   }
 
+  public static Customer getCustomer(String username) {
+    String sql = "select * from customers where username = ?";
+    Customer customer = null; 
+
+    try (Connection connection = connect(); 
+      PreparedStatement statement = connection.prepareStatement(sql)
+      ) {
+      statement.setString(1, username);
+      try (ResultSet resultSet = statement.executeQuery()) {
+       customer = new Customer(
+        resultSet.getInt("id"), 
+        resultSet.getString("name"), 
+        resultSet.getString("username"), 
+        resultSet.getString("password"), 
+        resultSet.getInt("account_id"));
+      }
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+
+    return customer;
+  }
+
   public static void main(String[] args) {
-    connect();
+    // connect();
+    Customer customer = getCustomer("twest8o@friendfeed.com");
+    System.err.println(customer.getName());
   }
 }
